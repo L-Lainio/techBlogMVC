cf3c8ebc6a58c4c26eb976adece79adafd803314
L-Lainio <arollainio@gmail.com>
2024-06-04 17:27:19 -0400

    updated and commented out areas in connection.js and commentsRoute.js

--- conf/connections.js
+++ conf/connections.js
@@ -1,21 +1,26 @@
 // Import Sequelize
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
 
 // Create a new Sequelize instance with the specified dialect
-const sequelize = new Sequelize('database', 'username', 'password', {
-	host: 'localhost',
-	dialect: 'mysql' // Specify the dialect here (e.g., 'mysql', 'postgres', 'sqlite')
-});
-
-// Test the connection
-async function testConnection() {
-	try {
-		await sequelize.authenticate();
-		console.log('Connection has been established successfully.');
-	} catch (error) {
-		console.error('Unable to connect to the database:', error);
-	}
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PW,
+		{
+			host: 'localhost',
+			dialect: 'mysql',
+			port: 3306,
+		},
+	);
 }
 
-// Call the testConnection function to test the connection
-testConnection();
+module.exports = sequelize;
+
+
+
