c0cb219469b6b77e0098fe886e31a455293413bb
L-Lainio <arollainio@gmail.com>
2024-06-04 19:18:55 -0400

    added .env prettier ignore and json file

--- conf/connections.js
+++ conf/connections.js
@@ -1,23 +1,16 @@
-// Import Sequelize
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-// Create a new Sequelize instance with the specified dialect
-if (process.env.JAWSDB_URL) {
-	sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-	sequelize = new Sequelize(
-		process.env.DB_NAME,
-		process.env.DB_USER,
-		process.env.DB_PW,
-		{
-			host: 'localhost',
-			dialect: 'sequelize',
-			port: 3001,
-		},
-	);
-}
+//Crea a new Sequelize instance with the specified dialect
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PW,
+	{
+		host: 'localhost',
+		dialect: 'postgres',
+		port: 3001,
+	},
+);
 
 module.exports = sequelize;
