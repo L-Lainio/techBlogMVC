e3177b7f35395760c86a2a59fcb745003d531e82
L-Lainio <arollainio@gmail.com>
2024-06-04 18:16:50 -0400

    updated middleware in server.js

--- server.js
+++ server.js
@@ -1,62 +1,45 @@
-// Local Modules
+const path = require('path');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
-// Third-Party Modules
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
-
-
 const sequelize = require('./config/connection');
+
 // Create a new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Initialize an instance of Express.js
 const app = express();
-// Specify on which port the Express.js server will run
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js as the default engine with custom helpers
-// const helpers = exphbs.create('helpers');
+const handlebars = exphbs.create({ helpers });
 
-// Sets up session and connect to our Sequelize db
-// Configure and link a session object with the sequelize store
+// Sets up the connect session through sequelize
 const sess = {
-    secret: 'Super secret secret',
-    // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property to our session options.
-    cookie: {
-        // maxAge sets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. The time should be given in milliseconds.
-        maxAge: 300000,
-        // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
-        httpOnly: true,
-        // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
-        secure: false,
-        // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
-        sameSite: 'strict',
-    },
+    secret: 'Nunyabusiness',
+    cookie: {},
     resave: false,
     saveUninitialized: true,
+
     // Sets up session store
     store: new SequelizeStore({
         db: sequelize,
     }),
-};
+},
+
 // Add express-session and store as Express.js middleware
 app.use(session(sess));
-
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', hbs.engine);
+app.use(express.json());
+app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
