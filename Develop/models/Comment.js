99113d04ed325ead7beaad49f86e316cf374bc33
Dave lazar <arollainio@gmail.com>
2024-08-10 14:18:08 -0400

    updated package.json and models files

--- Develop/models/Comment.js
+++ Develop/models/Comment.js
@@ -4,23 +4,17 @@ const {
 } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model { }
 
-Post.init({
+class Comment extends Model { }
+
+Comment.init({
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
     },
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1]
-        }
-    },
-    content: {
+    comment_text: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
@@ -29,17 +23,26 @@ Post.init({
     },
     user_id: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         references: {
             model: 'user',
             key: 'id'
         }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
     }
-},
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post'
-    })
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+})
 
-module.exports = Post;
+
+module.exports = Comment;
