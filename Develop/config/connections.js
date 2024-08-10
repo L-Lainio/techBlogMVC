8b8fb54f9d33e73c2f2723276d35c3c74db5905a
Dave lazar <arollainio@gmail.com>
2024-08-10 15:00:29 -0400

    corrected singular vs.plural connection(s)

--- Develop/config/connections.js
+++ Develop/config/connections.js
@@ -1,17 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
-    : new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'postgres',
-            port: 3001
-        }
-    );
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT
+    }
+);
 
 module.exports = sequelize;
--- Develop/server.js
+++ Develop/server.js
@@ -1,43 +1,32 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connections');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({
     helpers
 });
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-    secret: process.env.DB_SECRET,
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
-        expiration: 1000 * 60 * 30 // will expire after 30 minutes
-    })
-};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const sess = {
+    secret: 'Super secret secret', // Provide a secret here
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(routes);
 
-sequelize.sync();
+// Other middleware and routes
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
