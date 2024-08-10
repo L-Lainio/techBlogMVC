9eff6a42b178e0b28065c2669626b40aeb8ece29
Dave lazar <arollainio@gmail.com>
2024-08-10 16:36:15 -0400

    updater helper.js and handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,16 @@
-function formatDate(date) {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-    ).getFullYear()}`;
-}
-
-
+// Exporting an object with various utility functions
 module.exports = {
-    formatDate
-}
+
+    // Format Date Function: Converts a date to the format DD/MM/YY
+    format_date: (date) => {
+        return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${new Date(date).getFullYear().toString().substr(-2)}`;
+    },
+
+    // Format Plural Function: Adds an 's' to a word if the amount is not 1
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+        return word;
+    },
+};
