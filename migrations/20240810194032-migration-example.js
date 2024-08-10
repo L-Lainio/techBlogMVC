5122ad01678623ef9e36d17cfeb55ab5a970968d
Dave lazar <arollainio@gmail.com>
2024-08-10 15:45:52 -0400

    adding Sequelize CLI and migration

---
+++ migrations/20240810194032-migration-example.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { DataTypes } = require('@sequelize/core');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable('Person', {
+      name: DataTypes.STRING,
+      isBetaMember: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.dropTable('Person');
+  }
+};
